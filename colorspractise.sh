f4808d9afa117893cb8dbbdf4a0bb6e8f04ef444
parthiv <parthivadusumalli@gmail.com>
2025-01-04 22:53:41 +0530

    Commiting

--- colorspractise.sh
+++ colorspractise.sh
@@ -2,12 +2,12 @@
 
 RED='\e[31m'
 GREEN='\e[32m'
-RESET='\0e[31m'
+RESET='\e[31m'
 
 if [ 12 -eq 12 ];
    then
-        echo "$GREEN Condition Correct. $RESET"
+        echo -e "$GREEN Condition Correct. $RESET"
 else
-        echo "$RED Condition incorrect..$RESET"
+        echo -e "$RED Condition incorrect..$RESET"
         echo "Please check."
 fi
