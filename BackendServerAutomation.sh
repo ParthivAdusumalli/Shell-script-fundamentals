3ad32aeb22c42a3bb3f3b7334e31b1813899755b
parthiv <parthivadusumalli@gmail.com>
2025-01-10 11:18:25 +0530

    Updated

--- BackendServerAutomation.sh
+++ BackendServerAutomation.sh
@@ -3,29 +3,31 @@ RED="\e[31m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
 DEF="\e[0m"
-
-NODE_VER=$(node -v)
 DATE=$(date +%Y-%m-%d)
-if [ -d "/var/log/InstallationLogs/" ]; then
+if  test -d "/var/log/InstallationLogs/" ; then
        echo -e "$YELLOW Logs Directory exists..$DEF"     
 else
     echo -e "$GREEN Creating Logs Directory $DEF"
     mkdir /var/log/InstallationLogs/
 fi
-if [ "$NODE_VER" == "v20.17.0" ]; then
-   echo -e "$YELLOW The Current Node JS version is v20.17.0 $DEF"
-else 
-    echo -e "$GREEN Configuring the Node JS v20.17.0 $DEF"
-    dnf module disable nodejs -y &> /var/log/InstallationLogs/$DATE-Install-logs.log
-    dnf module enable nodejs:20 -y &>> /var/log/InstallationLogs/$DATE-Install-logs.log
-    dnf install nodejs -y &>> /var/log/InstallationLogs/$DATE-Install-logs.log
-    NODE_VER_RECHECK=$(node -v)
-    if [ "$NODE_VER_RECHECK" == "v20.17.0" ]; then
+if [ "$(node -v)" == "v20.17.0" ]; then
+   echo "Node JS version is already on v20.17.0"
+   echo "Type 'y' if you would like to re-install.. "
+   read "CHOICE"
+   if [ "$CHOICE" == "y" ]; then
+      echo "Installing Node JS v20.17.0"
+      dnf module disable nodejs -y &> /var/log/InstallationLogs/$DATE-Install-logs.log
+      dnf module enable nodejs:20 -y &>> /var/log/InstallationLogs/$DATE-Install-logs.log
+      dnf install nodejs -y &>> /var/log/InstallationLogs/$DATE-Install-logs.log
+      if [ "$(node -v)" == "v20.17.0" ]; then
        echo -e "$GREEN Successfully configured the nodejs v20 $DEF"
-    else 
+      else 
         echo -e "$RED Configuring Node JS v20 Failed..Check logs $DEF"
         exit 1
-   fi
+      fi
+   else 
+       echo "Skipping the Node.JS Installation."
+    fi
 fi
 
 if id "expense" &>/dev/null; then
@@ -40,68 +42,58 @@ else
         exit 1
     fi
 fi
-
-ls -l / | grep app
-
+rm -rf /app &>>/var/log/InstallationLogs/$DATE-Install-logs.log
 if [ $? -eq 0 ]; then
-   echo -e "$YELLOW The Directory app was present at root location(/) $DEF"
+   echo "Removed the already existing /app Directory... "
 else
-    echo -e "$YELLOW app directory missing creating the app directory at root location $DEF"
-    mkdir /app
-    ls -l / | grep app
-    if [ $? -eq 0 ]; then
-       echo -e "$GREEN Directory /app successfully created $DEF"
-   else
-       echo -e "$RED app directory cant be created..ERROR $DEF"
-      fi
+   echo "Directory Not exist.Creating Now"
+   mkdir /app
 fi
-ls -l /tmp | grep backend.zip
-if [ $? -ne 0 ]; then
-   echo -e "$YELLOW Backend.zip file Missing. Downloading the file..$DEF"
-   curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+ls -l / | grep app
+if [ $? -eq 0 ]; then
+   echo -e "$GREEN Directory /app Successfully created. $DEF"
 else
-    echo -e "$YELLOW Application Code Zip file present at /tmp/backend.zip $DEF"
+    echo -e "$RED Directory /app creating failed. $DEF"
+    exit 1
 fi
-
-cd /app
-PRESENTW_D=$(pwd)
-if [ "$PRESENTW_D" == "/app" ]; then
+if  test -f "/tmp/backend.zip" ; then
+    echo "backend.zip already exist. Deleting now and will be redownloaded."
+    rm -f /tmp/backend.zip
+    curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+    cd /app
+    echo "Unzipping the backend.zip at /app"
+    unzip /tmp/backend.zip
+    cd /app
+else
+    echo "Downloading the backend.zip file.."
+    curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+    cd /app
+    PRESENTW_D=$(pwd)
+    if [ "$PRESENTW_D" == "/app" ]; then
+       echo -e "$GREEN Current working directory is /app $DEF"
+       echo "Unzipping the backend.zip at /app"
+       unzip /tmp/backend.zip
+    else
+       echo -e "$RED Unable to change directory.$DEF"
+       exit 1
+    fi
+    cd /app
+fi
+if [ "$(pwd)" == "/app" ]; then
    echo -e "$GREEN Current working directory is /app $DEF"
 else
    echo -e "$RED Unable to change directory.$DEF"
    exit 1
 fi
-
-echo -e "$YELLOW Trying to Unzip the File content $DEF"
-
-cat /var/log/InstallationLogs/$DATE-Install-logs.log | grep "Archive:  /tmp/backend.zip"
-
+echo "Installing Node Package manager."
+npm install
 if [ $? -eq 0 ]; then
-   echo -e "$YELLOW Already Unzipping Finished..Cant unzip again..$DEF"
-else 
-    unzip /tmp/backend.zip
-fi
-
-cd /app
-PRESENTW_D=$(pwd)
-if [ "$PRESENTW_D" == "/app" ]; then
-   echo -e "$GREEN Current working directory is /app $DEF"
+   echo "Installing NPM Success"
 else
-   echo -e "$RED Unable to change directory. $DEF"
-   exit 1
+    echo "installing NPM Failed."
+    exit 1
 fi
-
-NPM_V=$(npm -v)
-
-if [ "$NPM_V" == "10.8.2" ]; then
-   echo -e "$YELLOW node package manager already installed..$DEF"
-else
-   echo -e "$GREEN Installing the npm.$DEF"
-   npm install
-fi
-
 grep -q "Environment=DB_HOST" /etc/systemd/system/backend.service
-
 if [ $? -eq 0 ]; then
    echo "$YELLOW backend service file already configured..Please check if required$DEF"
    cat /etc/systemd/system/backend.service
@@ -125,7 +117,14 @@ systemctl enable backend
 dnf list installed | grep mysql &>/dev/null
 
 if [ $? -eq 0 ]; then
-    echo -e "$YELLOW Mysql Command line client already installed..$DEF"
+    echo -e "$YELLOW Mysql Command line client already installed..Type y if you would like to redownload..$DEF"
+    read "CHOICE_2"
+    if [ "$CHOICE_2" == "y" ]; then
+            echo "Installing Mysql Command line client.."
+            dnf install mysql -y &>>/var/log/InstallationLogs/$DATE-Install-logs.log
+    else
+        echo "Skipping installation of Mysql.."
+   fi
 else
     echo -e "$YELLOW Mysql client not installed installing Now..$DEF"
     dnf install mysql -y &>>/var/log/InstallationLogs/$DATE-Install-logs.log
@@ -138,15 +137,14 @@ else
     echo -e "$GREEN Configuring Schema Completed $DEF" >> $DATE-Install-logs.log
 fi
 systemctl restart backend
-echo -e "$GREEN Adding Other dependencies...$DEF"
-   npm install mysql2
-systemctl status backend | grep "active (running)"
-if [ $? -eq 0 ];then
-   echo -e "$GREEN Backend Service up and running..$DEF"
-   else
-       echo -e "$RED Backend Service Still failing $DEF"
-       exit 1
-fi
+#echo -e "$GREEN Adding Other dependencies...$DEF"
+ #  npm install mysql2
+#systemctl status backend | grep "active (running)"
+#if [ $? -eq 0 ];then
+ #  echo -e "$GREEN Backend Service up and running..$DEF"
+  # else
+   #    echo -e "$RED Backend Service Still failing $DEF"
+    #   exit 1
+#fi
 systemctl daemon-reload
 systemctl restart backend
-echo -e "$GREEN Script completed successfully. Check $LOG_FILE for details.$DEF"
--- FrontEndServerAutomate.sh
+++ FrontEndServerAutomate.sh
@@ -43,75 +43,77 @@ systemctl start nginx &>>/dev/null
 
 Checking_Conf()
 {
+  $1
   if [ $? -eq 0 ]; then
-     echo -e "$GREEN$1 Nginx Web Server..$DEF"
+     echo -e "$GREEN$2 Nginx Web Server..$DEF"
   else 
       echo -e "$RED Failed to Start/enable the Nginx Web server.."
       echo -e "Terminating the Script..$DEF"
       exit 1
   fi
 }
-Checking_Conf Started
+Checking_Conf "systemctl start nginx" "Started"
 
-systemctl enable nginx
-
-Checking_Conf Enabled
+Checking_Conf "systemctl enable nginx" "Enabled"
 
 LOG_FILE="/var/log/InstallLogs/$DATE-Installation.log"
 
-# Check if the log entry indicating the files were removed already exists
-grep -q "Removing HTML Files for the first time" "$LOG_FILE" &>>/dev/null
+rm -rf /usr/share/nginx/html/* &>>"$LOG_FILE"
 if [ $? -eq 0 ]; then
-    echo "Files have already been configured in the Nginx directory; skipping deletion."
-else
-    rm -rf /usr/share/nginx/html/* &>>"$LOG_FILE"
-    if [ $? -eq 0 ]; then
-        # Log the message indicating successful removal
-        echo -e "$GREEN Removing HTML Files for the first time" >>"$LOG_FILE"
-        echo "HTML files in the Nginx directory have been successfully removed for the first time.$DEF"
+        echo -e "$GREEN Removing HTML Files$DEF" >>"$LOG_FILE"
     else
         echo "$RED Failed to remove the Nginx files; please check the logs for more details.$DEF" >&2
         exit 1
-    fi
 fi
-#Downloading the frontend Content of application
-if [ -f "/tmp/frontend.zip" ]; then
-   echo "Front End Zip file Already Present.."
+
+if test -f "/tmp/frontend.zip"; then
+         echo "frontend.zip already exists removing the zip file and redownloading.."
+         rm -f /tmp/frontend.zip
+         curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+         if [ $? -eq 0 ]; then
+            echo "Downloaded the Zip file content successfully.."
+         else 
+            echo "Downloading the Zip file failed.."
+            exit 1
+         fi
 else
-    echo -e "$GREEN Downloading the Front end Content.."
-    curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip 2>&1 | tee -a "$LOG_FILE"
-    echo -e "Unzipping the content $DEF"
-    cp /tmp/frontend.zip /usr/share/nginx/html
+    echo "Downloading the zip file.."
+    curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
     if [ $? -eq 0 ]; then
-       echo "Copied Zip file successfully"
+            echo "Downloaded the Zip file content successfully.."
     else 
-        echo "Copying files failed.."
+            echo "Downloading the Zip file failed.."
+            exit 1
     fi
-    unzip /tmp/frontend.zip
 fi
-
-if [ -f "/etc/nginx/default.d/expense.conf" ]; then
-   echo -e "$GREEN Nginx conf set up Already completed..$DEF"
+cd /usr/share/nginx/html
+PRE=$(pwd)
+if [ "$PRE" == "/usr/share/nginx/html" ]; then
+   echo "Directory changed to /usr/share/nginx/html"
+   echo "Unzipping the zip file content.."
+   unzip /tmp/frontend.zip
 else
-touch /etc/nginx/default.d/expense.conf
-
-echo "proxy_http_version 1.1;
-
+   echo "Failed to change the directory.."
+   exit 1
+fi
+if  test -f "/etc/nginx/default.d/expense.conf"; then
+    echo -e "$GREEN Nginx conf set up Already completed..Please check the configuration below$DEF"
+    cat /etc/nginx/default.d/expense.conf
+else
+    echo "Configuring the expense.conf file"
+    touch /etc/nginx/default.d/expense.conf
+    echo "proxy_http_version 1.1;
 location /api/ { proxy_pass http://10.1.2.203:8080/; }
-
 location /health {
   stub_status on;
   access_log off;
-}" >> /etc/nginx/default.d/expense.conf
-
-fi
-cd /usr/share/nginx/html
-PRE_WD=$(pwd)
-if [ "$PRE_WD" == "/usr/share/nginx/html" ]; then
-   echo "Present working directory is /usr/share/nginx/html"
-   unzip /usr/share/nginx/html/frontend.zip
+}" > /etc/nginx/default.d/expense.conf
+if [ $? -eq 0 ]; then
+   echo "Successfully configured the expense.conf"
 else 
-    echo "Changing directory failed.."
+   echo "Configuring the expense.conf failed.."
+   exit 1
+fi
 fi
 systemctl restart nginx
 echo -e "$GREEN Restarted the Nginx Service...$DEF"
